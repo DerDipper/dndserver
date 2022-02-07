3eb6b6587fc3de243125128723401e073e5a0d59
Stefan Dennig <stefan-dennig@web.de>
2022-02-07 21:15:47 +0000

    Added Creation of Game USer

--- setup_user.sh
+++ setup_user.sh
@@ -15,5 +15,10 @@ sudo chown $1:$1 /home/$1/foundrydl
 
 sudo cp foundry/docker-compose.yml /home/$1/foundry/docker-compose.yml
 sudo chown $1:$1 /home/$1/foundry/docker-compose.yml
+sudo mkdir -p /home/$1/foundrydata/Config
+sudo chown $1:$1 /home/$1/foundrydata/Config
+sudo cp foundry/options.json /home/$1/foundrydata/Config/options.json
+sudo chown $1:$1 /home/$1/foundrydata/Config/options.json
+
 fi
 
