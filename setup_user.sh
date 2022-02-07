5f10db2abc78cb33a8af1e9b6b4b87b26dc408a9
Stefan Dennig <stefan-dennig@web.de>
2022-02-07 20:10:32 +0000

    Added foundry stuff

--- setup_user.sh
+++ setup_user.sh
@@ -4,3 +4,16 @@ sudo adduser $1
 sudo usermod -aG sudo $1
 sudo usermod -aG docker $1
 
+if [ "$2" = "-game" ]; then
+echo "is DM"
+sudo mkdir -p /home/$1/foundry
+sudo chown $1:$1 /home/$1/foundry
+sudo mkdir -p /home/$1/foundrydata
+sudo chown $1:$1 /home/$1/foundrydata
+sudo mkdir -p /home/$1/foundrydl
+sudo chown $1:$1 /home/$1/foundrydl
+
+sudo cp foundry/docker-compose.yml /home/$1/foundry/docker-compose.yml
+sudo chown $1:$1 /home/$1/foundry/docker-compose.yml
+fi
+
